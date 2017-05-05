cb358b6de9fc2b248c82daba938d9b95fb07461b
wcgLR <wuchaogang88@163.com>
2017-05-05 10:39:34 +0800

    Initial Commit

---
+++ WCGServiceBar/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WCGServiceBar
+//
+//  Created by Wcg on 2017/5/5.
+//  Copyright © 2017年 吴朝刚. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ WCGServiceBar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WCGServiceBar
+//
+//  Created by Wcg on 2017/5/5.
+//  Copyright © 2017年 吴朝刚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
